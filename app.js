6c08750e0bf99598612aa6443a637bb5083cfc32
Unknown <achiche.kevin@hotmail.fr>
2020-10-26 14:28:48 +0100

    First Push

---
+++ app.js
@@ -0,0 +1,12 @@
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function main() {
+  // Récupérer l'adresse IP du client qui ouvre la page :  https://api.ipify.org?fomat=json
+  // Récupérer la ville grace a l'adresse IP : http://freegeoip.net/json/adresseIPenQuestion
+  // Récupérer les infos météo grace a la ville : http://api.openweathermap.org/data/2.5/weather?q=VillleEnQuestion&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric
+  // Afficher les informations sur la page
+}
+
+main();
